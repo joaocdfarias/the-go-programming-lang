7cb2b6942cc7f1ab79dc48f49f4b2583980d2ef5
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2024-10-06 23:39:08 -0300

    feat: unused variables

--- main.go
+++ main.go
@@ -5,13 +5,8 @@ import (
 )
 
 func main() {
-	var x int = 10
-	var y float64 = 30.2
-	var z float64 = float64(x) + y
-	var d int = x + int(y)
-
-	fmt.Println(x) // 10
-	fmt.Println(y) // 30.2
-	fmt.Println(z) // 10.0 + 30.2 = 40.2
-	fmt.Println(d) // 10 + 30 = 40
+	x := 10
+	x = 20
+	fmt.Println(x)
+	x = 30
 }
