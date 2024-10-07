8d507046a9bf37f736231b954505bfeb9de002a2
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2024-10-06 23:24:12 -0300

    feat(ex2.2-p47): types in go

--- main.go
+++ main.go
@@ -1,37 +1,17 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 )
 
 func main() {
-	counts := make(map[string]int)
-	files := os.Args[1:]
-	if len(files) == 0 {
-		countLines(os.Stdin, counts)
-	} else {
-		for _, arg := range files {
-			f, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
-				continue
-			}
-			countLines(f, counts)
-			f.Close()
-		}
-	}
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
-		}
-	}
-}
+	var x int = 10
+	var y float64 = 30.2
+	var z float64 = float64(x) + y
+	var d int = x + int(y)
 
-func countLines(f *os.File, counts map[string]int) {
-	input := bufio.NewScanner(f)
-	for input.Scan() {
-		counts[input.Text()]++
-	}
+	fmt.Println(x) // 10
+	fmt.Println(y) // 30.2
+	fmt.Println(z) // 10.0 + 30.2 = 40.2
+	fmt.Println(d) // 10 + 30 = 40
 }
